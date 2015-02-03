eee9858c96253bfc077a6f847fda20526ea91c3c
martinlj10 <martinlj10@hotmail.com>
2015-02-03 19:20:39 -0300

    Prototipo

---
+++ SGRespuestos/ClassLibrary1/ModeloProveedores.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models
+{
+   public class ModeloProveedores
+    {
+       string _razonSocialProv;
+       string razonSocialProv
+       {
+           set { this._razonSocialProv = value; }
+           get { return _razonSocialProv; }
+       }
+
+       string _cuitProv;
+       string cuitProv
+       {
+           get { return _cuitProv; }
+           set { _cuitProv = value; }
+       }
+       
+       string _direccionProv;
+       string direccionProv
+       {
+           get { return _direccionProv; }
+           set { _direccionProv = value; }
+       }
+       
+       string _ciudadProv;
+       string ciudadProv
+       {
+           get { return _ciudadProv; }
+           set { _ciudadProv = value; }
+       }
+
+       string _provinciaProv;
+       string provinciaProv
+       {
+           get { return _provinciaProv; }
+           set { _provinciaProv = value; }
+
+       }
+
+       string _codigoPostalProv;
+       string codigoPostalProv
+       {
+           get { return _codigoPostalProv; }
+
+           set { _codigoPostalProv = value; }
+       }
+       
+       string _obsProv;
+       string obsProv
+       {
+           get { return _obsProv; }
+           set { _obsProv = value; }
+       }
+
+       
+       
+       
+    }
+}
---
+++ SGRespuestos/Datos/Conexion.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Datos
+{
+    public class Conexion
+    {
+        public void OpenConnection()
+        {
+            
+        }
+        public void CloseConnection()
+        { }
+
+    }
+}
